2e6be6da890b86f6c253299c72ec74d71365d6df
susovanpatra00 <susovanpatra00@gmail.com>
2024-05-10 19:14:56 +0530

    All Done

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask,render_template,request
+import pickle
+
+app = Flask(__name__)
+
+
+pipe = pickle.load(open("Model/BOW_NB_model.pkl","rb"))
+
+@app.route('/', methods=["GET","POST"])
+def main_function():
+    if request.method == "POST":
+        text = request.form
+        emails = text['sms']
+        print(emails)
+        
+        list_email = [emails]
+        output = pipe.predict(list_email)[0]
+        print(output)
+
+
+        return render_template("show.html", prediction = output)
+    
+    else:
+        return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
